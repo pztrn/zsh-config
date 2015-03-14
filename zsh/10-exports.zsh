21496f2b17cecb2c2440fbb0ebf828b4e66fed85
pztrn <pztrn@pztrn.name>
2015-03-14 20:39:49 +0500

    Load exports from userdir also.

--- zsh/10-exports.zsh
+++ zsh/10-exports.zsh
@@ -1,3 +1,8 @@
 for config_file in `ls ${CONFIG_PATH}/zsh/exports | sort | grep zsh`; do
     source ${CONFIG_PATH}/zsh/exports/${config_file}
 done
+
+for config_file in `ls ${HOME}/.zsh-config.d/exports | sort | grep zsh`; do
+    source ${HOME}/.zsh-config.d/exports/${config_file}
+done
+
