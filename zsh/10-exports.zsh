8a2c4139ab0b050a90409f812c7c9c543ae7d719
pztrn <pztrn@pztrn.name>
2015-03-21 16:34:10 +0500

    Exports fix, it should not produce warnings.

--- zsh/10-exports.zsh
+++ zsh/10-exports.zsh
@@ -2,9 +2,11 @@ for config_file in `ls ${CONFIG_PATH}/zsh/exports | sort`; do
     source ${CONFIG_PATH}/zsh/exports/${config_file}
 done
 
-for config_file in `ls ${HOME}/.zsh-config.d/exports | sort`; do
-    if [ -f ${HOME}/.zsh-config.d/exports/${config_file} ]; then
-        source ${HOME}/.zsh-config.d/exports/${config_file}
-    fi
-done
+if [ -d ${HOME}/.zsh-config.d/exports ]; then
+    for config_file in `ls ${HOME}/.zsh-config.d/exports | sort`; do
+        if [ -f ${HOME}/.zsh-config.d/exports/${config_file} ]; then
+            source ${HOME}/.zsh-config.d/exports/${config_file}
+        fi
+    done
+fi
 
