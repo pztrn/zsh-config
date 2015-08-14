6314db1861dfebd72688da1f4bf6da32e4a6f494
pztrn <pztrn@pztrn.name>
2015-08-14 21:43:54 +0500

    Export JAVA options only if we have X session running.

--- zsh/exports/70-java-options.zsh
+++ zsh/exports/70-java-options.zsh
@@ -1,4 +1,7 @@
 # Java-specific exports.
 # More or less pretty fonts in Java programs.
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-export JAVA_FONTS=/usr/share/fonts/TTF
+# It should be exported only if we have X session running.
+if [ ! -z ${DISPLAY} ]; then
+    export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+    export JAVA_FONTS=/usr/share/fonts/TTF
+fi
