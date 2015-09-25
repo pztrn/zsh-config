5047f866e9b3650770c565f856b6f4ffdd818f22
pztrn <pztrn@pztrn.name>
2015-09-26 02:12:54 +0500

    Java options should only be exported on Linux.

--- zsh/exports/70-java-options.zsh
+++ zsh/exports/70-java-options.zsh
@@ -1,7 +1,10 @@
 # Java-specific exports.
 # More or less pretty fonts in Java programs.
 # It should be exported only if we have X session running.
-if [ ! -z ${DISPLAY} ]; then
-    export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-    export JAVA_FONTS=/usr/share/fonts/TTF
+# Also, these settings should be applied only on Linux.
+if [[ "${OS}" == "Linux" ]]; then
+    if [ ! -z ${DISPLAY} ]; then
+        export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+        export JAVA_FONTS=/usr/share/fonts/TTF
+    fi
 fi
