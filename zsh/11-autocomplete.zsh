f62e3c95c692e3f1a372cb8325865d1cba37f7b1
Stanislav N. aka pztrn <pztrn@pztrn.name>
2022-02-14 23:10:34 +0500

    Fix tmux window naming when SSHing and fix Darwin/arm64 completions loading.

--- zsh/11-autocomplete.zsh
+++ zsh/11-autocomplete.zsh
@@ -1,6 +1,11 @@
+# Mac related - ensure that homebrew path on arm64 is added to fpath,
+# otherwise autocompletions from installed packages won't work.
+if [ "${OS}" = "Darwin" ] && [ "${OS_ARCH}" = "arm64" ]; then
+	export fpath=(${fpath} /opt/homebrew/share/zsh/site-functions)
+fi
+
 autoload -Uz compinit
 compinit
 zstyle ':completion:*' menu select=2
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
